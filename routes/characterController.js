5c46a6db40eb612c9f45f8bb5300ca42d077e63c
sillah2010 <macktastic2014@gmail.com>
2017-10-02 15:11:54 -0400

    Updated basic MVP

--- routes/characterController.js
+++ routes/characterController.js
@@ -5,16 +5,42 @@ const Schema = require("../db/schema.js");
 const PlayerModel = Schema.PlayerModel;
 const CharacterModel = Schema.CharacterModel;
 
-router.post('/', (req, res) => {
+router.get('/new', (req, res) => {
     const playerId = req.params.playerId
-    const newCharacters = req.body;
     PlayerModel.findById(playerId)
         .then((player) => {
-            player.characters.push(newCharacters)
-            return player.save()
+            CharacterModel.find({}).then((characters => {
+                res.render('characters/new', {
+                    characters: characters,
+                    player: player
+                })
+            }))
         })
+})
+
+router.post('/', (req, res) => {
+    const playerId = req.params.playerId
+    const newCharacter1Id = req.body.character1
+    const newCharacter2Id = req.body.character2
+    const newCharacter3Id = req.body.character3
+
+    PlayerModel.findById(playerId)
         .then((player) => {
-            res.redirect(`/players/${playerId}`)
+            CharacterModel.find({}).or([
+                { "_id": newCharacter1Id },
+                { "_id": newCharacter2Id },
+                { "_id": newCharacter3Id }])
+                .then((characters) => {
+                    console.log(characters)
+                    player.characters = characters;
+                    return player.save()
+                })
+                .then(() => {
+                    res.redirect(`/players/${playerId}`)
+                })
+        })
+        .catch((error) => {
+            console.log(error)
         })
 
 })
@@ -64,14 +90,31 @@ router.put('/:character1Id&:character2Id&:character3Id/', (req, res) => {
         })
 
 
+
 })
 
-module.exports = router
+router.get('/:character1Id&:character2Id&:character3Id/delete', (req, res) => {
 
-   // CharacterModel.find({
-            //     "$in": [{
-            //         "_id": newCharacter1Id,
-            //         "_id": newCharacter2Id,
-            //         "_id": newCharacter3Id
-            //     }]
-            // })
+    const playerId = req.params.playerId
+    const character1Id = req.params.character1Id
+    const character2Id = req.params.character2Id
+    const character3Id = req.params.character3Id
+
+    PlayerModel.findById(playerId)
+        .then((player) => {
+
+            const character1 = player.characters.id(character1Id).remove()
+            const character2 = player.characters.id(character2Id).remove()
+            const character3 = player.characters.id(character3Id).remove()
+            return player.save();
+        })
+        .then(() => {
+            res.redirect(`/players/${playerId}`)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+
+module.exports = router
